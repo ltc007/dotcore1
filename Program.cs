4e87cca881d03c6715035496e431f3e594ccb198
ntecce <cce@DESKTOP-RAVQMSA>
2020-01-02 12:24:43 +0800

    df ：

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace app01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("sdf");
+            sb.Append(124);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
